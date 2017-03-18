5f1a2d5356a68897669aba6626b543c7deab2414
edo_m18 <edo.m18@gmail.com>
2017-03-19 03:15:07 +0900

    Added to use a tree manager for MonoBehaviour.

---
+++ Assets/Scripts/LinearTreeBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearTreeBehaviour : MonoBehaviour
+{
+    private LinearTreeManager<GameObject> _manager;
+
+	void Start()
+    {
+        int level = 4;
+        float left = 10f;
+        float top = 10f;
+        float right = 100f;
+        float bottom = 100f;
+        _manager = new LinearTreeManager<GameObject>(level, left, top, right, bottom);
+	}
+	
+	void Update()
+    {
+		
+	}
+}
