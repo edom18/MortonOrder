33d880d61272140d8bac6d03569684908d319a59
edo_m18 <edo.m18@gmail.com>
2017-03-21 03:24:12 +0900

    Added some codes as adding objects to the manager.

--- Assets/Scripts/LinearTreeBehaviour.cs
+++ Assets/Scripts/LinearTreeBehaviour.cs
@@ -6,18 +6,47 @@ public class LinearTreeBehaviour : MonoBehaviour
 {
     private LinearTreeManager<GameObject> _manager;
 
+    [SerializeField]
+    private GameObject _object1;
+
+    [SerializeField]
+    private GameObject _object2;
+
+    [SerializeField]
+    private GameObject _object3;
+
+    [SerializeField]
+    private GameObject _object4;
+
 	void Start()
     {
+        // Max Level
         int level = 4;
-        float left = 10f;
-        float top = 10f;
+
+        float left = 0f;
+        float top = 0f;
         float right = 100f;
         float bottom = 100f;
         _manager = new LinearTreeManager<GameObject>(level, left, top, right, bottom);
+
+        // オブジェクトを仮登録してみる
+        RegisterObjects();
 	}
-	
-	void Update()
+
+    /// <summary>
+    /// オブジェクトを仮に登録してみる
+    /// </summary>
+    void RegisterObjects()
     {
-		
-	}
+        TreeData<GameObject> data1 = new TreeData<GameObject>(_object1);
+        TreeData<GameObject> data2 = new TreeData<GameObject>(_object2);
+        TreeData<GameObject> data3 = new TreeData<GameObject>(_object3);
+        TreeData<GameObject> data4 = new TreeData<GameObject>(_object4);
+
+        // オブジェクトを登録のテスト
+        _manager.Register(10f, 15f, 40f, 45f, data1);
+        _manager.Register(45f, 30f, 55f, 40f, data2);
+        _manager.Register(76f, 26f, 86f, 36f, data3);
+        _manager.Register(55f, 55f, 70f, 70f, data4);
+    }
 }
