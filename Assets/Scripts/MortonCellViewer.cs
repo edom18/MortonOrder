dc6ebf40fa27833aa2e5d797ad9eb6657c3ec7ac
edo_m18 <edo.m18@gmail.com>
2017-03-25 11:26:54 +0900

    Some public members are changed to properties.

--- Assets/Scripts/MortonCellViewer.cs
+++ Assets/Scripts/MortonCellViewer.cs
@@ -2,27 +2,88 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class MortonCellViewer : MonoBehaviour
 {
-    public float Width;
-    public float Height;
-    public float Depth;
-    public int Division;
+    #region Variables
+    private float _width;
+    public float Width
+    {
+        get
+        {
+            return _width;
+        }
+        set
+        {
+            _width = value;
+            UpdateCells();
+        }
+    }
+
+    private float _height;
+    public float Height
+    {
+        get
+        {
+            return _height;
+        }
+        set
+        {
+            _height = value;
+            UpdateCells();
+        }
+    }
+
+
+    private float _depth;
+    public float Depth
+    {
+        get
+        {
+            return _depth;
+        }
+        set
+        {
+            _depth = value;
+            UpdateCells();
+        }
+    }
+
+    private int _division;
+    public int Division
+    {
+        get
+        {
+            return _division;
+        }
+        set
+        {
+            _division = value;
+            UpdateCells();
+        }
+    }
 
     private float _unitWidth;
     private float _unitHeight;
     private float _unitDepth;
+    private int _halfDivision;
 
     private Color _normalColor = new Color(1f, 0, 0, 0.5f);
     private Color _centerColor = new Color(0, 0, 1f, 1f);
+    #endregion Variables
 
     void Start()
+    {
+        UpdateCells();
+    }
+
+    void UpdateCells()
     {
         // ひとつの区間の単位
         _unitWidth = Width / Division;
         _unitHeight = Height / Division;
         _unitDepth = Depth / Division;
+
+        _halfDivision = Division / 2;
     }
 
     /// <summary>
@@ -30,17 +91,20 @@ public class MortonCellViewer : MonoBehaviour
     /// </summary>
     void OnDrawGizmos()
     {
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+
         Vector3 tow = transform.right * Width;
         Vector3 toh = transform.up * Height;
         Vector3 tod = transform.forward * Depth;
-
-        int halfDivision = Division / 2;
 
         for (int i = 0; i <= Division; i++)
         {
             for (int j = 0; j <= Division; j++)
             {
-                if (i == halfDivision || j == halfDivision)
+                if (i == _halfDivision || j == _halfDivision)
                 {
                     Gizmos.color = _centerColor;
                 }
@@ -59,7 +123,7 @@ public class MortonCellViewer : MonoBehaviour
         {
             for (int j = 0; j <= Division; j++)
             {
-                if (i == halfDivision || j == halfDivision)
+                if (i == _halfDivision || j == _halfDivision)
                 {
                     Gizmos.color = _centerColor;
                 }
@@ -78,7 +142,7 @@ public class MortonCellViewer : MonoBehaviour
         {
             for (int j = 0; j <= Division; j++)
             {
-                if (i == halfDivision || j == halfDivision)
+                if (i == _halfDivision || j == _halfDivision)
                 {
                     Gizmos.color = _centerColor;
                 }
