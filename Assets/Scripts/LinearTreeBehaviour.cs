71ebccb991819fe40110a66db26aba448809e123
edo_m18 <edo.m18@gmail.com>
2017-03-26 23:22:31 +0900

    Added offset.

--- Assets/Scripts/LinearTreeBehaviour.cs
+++ Assets/Scripts/LinearTreeBehaviour.cs
@@ -4,7 +4,48 @@ using UnityEngine;
 
 public class LinearTreeBehaviour : MonoBehaviour
 {
+    #region SerializeField
+    [SerializeField]
+    private int _level = 3;
+
+    [SerializeField]
+    private float _left = 0f;
+
+    [SerializeField]
+    private float _top = 0f;
+
+    [SerializeField]
+    private float _right = 10f;
+
+    [SerializeField]
+    private float _bottom = 10f;
+
+    [SerializeField]
+    private float _front = 0f;
+
+    [SerializeField]
+    private float _back = 10f;
+    #endregion SerializeField
+
+    #region Variables
     private LinearTreeManager<GameObject> _manager;
+    private MortonCellViewer _cellViewer;
+    private MortonCellViewer CellViewer
+    {
+        get
+        {
+            if (_cellViewer == null)
+            {
+                MortonCellViewer viewer = GetComponent<MortonCellViewer>();
+                if (viewer == null)
+                {
+                    viewer = gameObject.AddComponent<MortonCellViewer>();
+                }
+                _cellViewer = viewer;
+            }
+            return _cellViewer;
+        }
+    }
 
     [SerializeField]
     private GameObject _object1;
@@ -17,19 +58,40 @@ public class LinearTreeBehaviour : MonoBehaviour
 
     [SerializeField]
     private GameObject _object4;
+    #endregion Variables
+
+
+    #region MonoBehaviour
+    void OnValidate()
+    {
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+
+        CellViewer.Left = _left;
+        CellViewer.Right = _right;
+        CellViewer.Top = _top;
+        CellViewer.Bottom = _bottom;
+        CellViewer.Front = _front;
+        CellViewer.Back = _back;
+        CellViewer.Division = 1 << _level;
+    }
+
+    void Awake()
+    {
+        CellViewer.Left = _left;
+        CellViewer.Right = _right;
+        CellViewer.Top = _top;
+        CellViewer.Bottom = _bottom;
+        CellViewer.Front = _front;
+        CellViewer.Back = _back;
+        CellViewer.Division = 1 << _level;
+    }
 
 	void Start()
     {
-        // Max Level
-        int level = 3;
-
-        float left = 0f;
-        float top = 0f;
-        float right = 100f;
-        float bottom = 100f;
-        float front = 0f;
-        float back = 100f;
-        _manager = new LinearTreeManager<GameObject>(level, left, top, right, bottom, front, back);
+        _manager = new LinearTreeManager<GameObject>(_level, _left, _top, _right, _bottom, _front, _back);
 
         // オブジェクトを仮登録してみる
         RegisterObjects();
@@ -38,6 +100,25 @@ public class LinearTreeBehaviour : MonoBehaviour
         List<GameObject> collisionList = new List<GameObject>();
        _manager.GetAllCollisionList(collisionList);
 	}
+    #endregion MonoBehaviour
+
+    /// <summary>
+    /// ゲームオブジェクトを登録する
+    /// </summary>
+    /// <param name="target">ターゲットのゲームオブジェクト</param>
+    void RegisterObject(GameObject target)
+    {
+        TreeData<GameObject> data = new TreeData<GameObject>(target);
+        MeshFilter filter = target.GetComponent<MeshFilter>();
+        Bounds bounds = filter.mesh.bounds;
+
+        _manager.Register(bounds, data);
+    }
+
+    void UnregisterObject(GameObject target)
+    {
+
+    }
 
     /// <summary>
     /// オブジェクトを仮に登録してみる
@@ -49,10 +130,12 @@ public class LinearTreeBehaviour : MonoBehaviour
         TreeData<GameObject> data3 = new TreeData<GameObject>(_object3);
         TreeData<GameObject> data4 = new TreeData<GameObject>(_object4);
 
+        RegisterObject(_object1);
+
         // オブジェクトを登録のテスト
-        _manager.Register(10f, 15f, 40f, 45f, 0f, 30f, data1);
-        _manager.Register(45f, 30f, 55f, 40f, 0f, 10f, data2);
-        _manager.Register(76f, 26f, 86f, 36f, 0f, 10f, data3);
-        _manager.Register(55f, 55f, 70f, 70f, 0f, 15f, data4);
+        //_manager.Register(10f, 15f, 40f, 45f, 0f, 30f, data1);
+        //_manager.Register(45f, 30f, 55f, 40f, 0f, 10f, data2);
+        //_manager.Register(76f, 26f, 86f, 36f, 13f, 23f, data3);
+        //_manager.Register(55f, 55f, 70f, 70f, 0f, 15f, data4);
     }
 }
--- Assets/Scripts/MortonCellViewer.cs
+++ Assets/Scripts/MortonCellViewer.cs
@@ -5,48 +5,99 @@ using UnityEngine;
 public class MortonCellViewer : MonoBehaviour
 {
     #region Variables
+    private float _left;
+    public float Left
+    {
+        get
+        {
+            return _left;
+        }
+        set
+        {
+            _left = value;
+            _width = _right - _left;
+            UpdateCells();
+        }
+    }
+
+    private float _right;
+    public float Right
+    {
+        get
+        {
+            return _right;
+        }
+        set
+        {
+            _right = value;
+            _width = _right - _left;
+            UpdateCells();
+        }
+    }
+
+    private float _top;
+    public float Top
+    {
+        get
+        {
+            return _top;
+        }
+        set
+        {
+            _top = value;
+            _height = _top - _bottom;
+            UpdateCells();
+        }
+    }
+
+    private float _bottom;
+    public float Bottom
+    {
+        get
+        {
+            return _bottom;
+        }
+        set
+        {
+            _bottom = value;
+            _height = _top - _bottom;
+            UpdateCells();
+        }
+    }
+
+    private float _front;
+    public float Front
+    {
+        get
+        {
+            return _front;
+        }
+        set
+        {
+            _front = value;
+            _depth = _back - _front;
+            UpdateCells();
+        }
+    }
+
+    private float _back;
+    public float Back
+    {
+        get
+        {
+            return _back;
+        }
+        set
+        {
+            _back = value;
+            _depth = _back - _front;
+            UpdateCells();
+        }
+    }
+
     private float _width;
-    public float Width
-    {
-        get
-        {
-            return _width;
-        }
-        set
-        {
-            _width = value;
-            UpdateCells();
-        }
-    }
-
     private float _height;
-    public float Height
-    {
-        get
-        {
-            return _height;
-        }
-        set
-        {
-            _height = value;
-            UpdateCells();
-        }
-    }
-
-
     private float _depth;
-    public float Depth
-    {
-        get
-        {
-            return _depth;
-        }
-        set
-        {
-            _depth = value;
-            UpdateCells();
-        }
-    }
 
     private int _division;
     public int Division
@@ -79,9 +130,9 @@ public class MortonCellViewer : MonoBehaviour
     void UpdateCells()
     {
         // ひとつの区間の単位
-        _unitWidth = Width / Division;
-        _unitHeight = Height / Division;
-        _unitDepth = Depth / Division;
+        _unitWidth = _width / Division;
+        _unitHeight = _height / Division;
+        _unitDepth = _depth / Division;
 
         _halfDivision = Division / 2;
     }
@@ -96,61 +147,49 @@ public class MortonCellViewer : MonoBehaviour
             return;
         }
 
-        Vector3 tow = transform.right * Width;
-        Vector3 toh = transform.up * Height;
-        Vector3 tod = transform.forward * Depth;
+        Vector3 tow = transform.right * _width;
+        Vector3 toh = transform.up * _height;
+        Vector3 tod = transform.forward * _depth;
 
+        // XY平面
         for (int i = 0; i <= Division; i++)
         {
             for (int j = 0; j <= Division; j++)
             {
-                if (i == _halfDivision || j == _halfDivision)
-                {
-                    Gizmos.color = _centerColor;
-                }
-                else
-                {
-                    Gizmos.color = _normalColor;
-                }
-                Vector3 offset = (transform.right * _unitWidth * i) + (transform.up * _unitHeight * j);
+                bool isCenter = (i == _halfDivision || j == _halfDivision);
+                Gizmos.color = isCenter ? _centerColor : _normalColor;
+
+                Vector3 offset = (transform.right * (_unitWidth * i + _left)) + (transform.up * (_unitHeight * j + _bottom)) + (transform.forward * _front);
                 Vector3 from = transform.position + offset;
                 Vector3 to = from + tod;
                 Gizmos.DrawLine(from, to);
             }
         }
 
+        // YZ平面
         for (int i = 0; i <= Division; i++)
         {
             for (int j = 0; j <= Division; j++)
             {
-                if (i == _halfDivision || j == _halfDivision)
-                {
-                    Gizmos.color = _centerColor;
-                }
-                else
-                {
-                    Gizmos.color = _normalColor;
-                }
-                Vector3 offset = (transform.forward * _unitDepth * i) + (transform.up * _unitHeight * j);
+                bool isCenter = (i == _halfDivision || j == _halfDivision);
+                Gizmos.color = isCenter ? _centerColor : _normalColor;
+
+                Vector3 offset = (transform.forward * (_unitDepth * i + _front)) + (transform.up * (_unitHeight * j + _bottom)) + (transform.right * _left);
                 Vector3 from = transform.position + offset;
                 Vector3 to = from + tow;
                 Gizmos.DrawLine(from, to);
             }
         }
 
+        // XZ平面
         for (int i = 0; i <= Division; i++)
         {
             for (int j = 0; j <= Division; j++)
             {
-                if (i == _halfDivision || j == _halfDivision)
-                {
-                    Gizmos.color = _centerColor;
-                }
-                else
-                {
-                    Gizmos.color = _normalColor;
-                }
-                Vector3 offset = (transform.forward * _unitDepth * i) + (transform.right * _unitWidth * j);
+                bool isCenter = (i == _halfDivision || j == _halfDivision);
+                Gizmos.color = isCenter ? _centerColor : _normalColor;
+
+                Vector3 offset = (transform.forward * (_unitDepth * i + _front)) + (transform.right * (_unitWidth * j + _left)) + (transform.up * _bottom);
                 Vector3 from = transform.position + offset;
                 Vector3 to = from + toh;
                 Gizmos.DrawLine(from, to);
