675421f4e1f6a1aa92f639c32d76b47b9c434622
k-hiruma <k-hiruma@colopl.co.jp>
2017-03-28 10:28:36 +0900

    Added morton agent class.

---
+++ Assets/Scripts/MortonAgent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An agent will be in octree.
+/// </summary>
+public class MortonAgent : MonoBehaviour
+{
+    public TreeData<GameObject> TreeData { get; private set; }
+
+    private Collider _collider;
+    // Bounds like AABB of this game object.
+    public Bounds Bounds
+    {
+        get
+        {
+            if (_collider == null)
+            {
+                _collider = GetComponent<Collider>();
+            }
+            return _collider.bounds;
+        }
+    }
+
+    #region MonoBehaviour
+    void Awake()
+    {
+        TreeData = new TreeData<GameObject>(gameObject);
+    }
+
+    void OnDestroy()
+    {
+        TreeData.Remove();
+    }
+    #endregion MonoBehaviour
+
+    public void UpdateAgent()
+    {
+
+    }
+}
